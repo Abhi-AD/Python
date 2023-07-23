c91bc342863db61cd9d3959c91a09342bb00343b
Abhishek Dangi <72682273+Abhi-AD@users.noreply.github.com>
2023-07-23 06:50:08 +0545

    Update Marksheet.py

--- Marksheet.py
+++ Marksheet.py
@@ -1,27 +1,32 @@
-# Function to take user input for subject marks
-student_range = int(input("Enter a save data number: "))
+import json
 
-for i in range(student_range):
-    name = input("Enter a student name: ")
-    print("Slow the student name:   ")
-    def get_subject_marks():
-        subject_marks = []
-        num_subjects = int(input("Enter the number of subjects: "))
+def get_subject_marks():
+    subject_marks = []
+    num_subjects = int(input("Enter the number of subjects: "))
+    
+    for i in range(num_subjects):
+        subject_name = input(f"Enter Subject {i+1} name: ")
+        marks = int(input(f"Enter marks for {subject_name}: "))
+        subject_marks.append((subject_name, marks))
+    
+    return subject_marks
+
+def main():
+    student_range = int(input("Enter the number of students: "))
+    data = []
+
+    for i in range(student_range):
+        name = input("Enter a student name: ")
+        print(f"**---Enter the subject marks for student: {name}---**")
         
-        for i in range(num_subjects):
-            subject_name = input(f"Enter Subject {i+1} name: ")
-            marks = int(input(f"Enter marks for {subject_name}: "))
-            subject_marks.append((subject_name, marks))
-        
-        return subject_marks
+        # Get user input for subject marks
+        subject_marks = get_subject_marks()
 
-    # Get user input for subject marks
-    subject_marks = get_subject_marks()
+        data.append({"name": name, "marks": subject_marks})
 
-    print("\nOriginal list of tuples:")
-    print(subject_marks)
+    # Save the data to a JSON file
+    with open("student_data.json", "w") as file:
+        json.dump(data, file)
 
-    subject_marks.sort(key=lambda x: x[1])
-
-    print("\nSorting the List of Tuples based on marks:")
-    print(subject_marks)
+if __name__ == "__main__":
+    main()
