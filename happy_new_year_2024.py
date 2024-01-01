8e97630906a72c16a87c21d3e835ab8acefaabca
Abhishek Dangi <72682273+Abhi-AD@users.noreply.github.com>
2024-01-01 22:38:23 +0545

    Happy New years 2024

---
+++ happy_new_year_2024.py
@@ -0,0 +1,13 @@
+from colorama import Fore
+
+def heart_shape(msg='Happy New Year 2024! 🎉'):
+    lines = []
+    for y in range(15, -15, -1):
+        line = ""
+        for x in range(-30, 30):
+            f = ((x * 0.05) ** 2 + (y * 0.1) ** 2 - 1) ** 3 - (x * 0.05) ** 2 * (y * 0.1) ** 3
+            line += msg[(x - y) % len(msg)] if f <= 0 else " "
+        lines.append(line.center(80))  # Center the line to make it look better
+    print(Fore.BLUE + "\n".join(lines))
+
+heart_shape()
