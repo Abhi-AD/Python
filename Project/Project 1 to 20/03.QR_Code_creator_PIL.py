bb2dcd752e30f24a06d542e1eca3a59452fe0768
Abhi-AD <dangiabhi332@gmail.com>
2025-01-22 21:55:39 +0545

    add the data input create a qr code design cutomized

---
+++ Project/Project 1 to 20/03.QR_Code_creator_PIL.py
@@ -0,0 +1,15 @@
+import qrcode
+from PIL import Image
+import qrcode.constants
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=4,
+)
+qrcodedata = input("Enter the Url: ")
+qr.add_data(qrcodedata)
+qr.make(fit=True)
+img = qr.make_image(fill_color="white", back_color="blue")
+img.save("qrcode.png")
